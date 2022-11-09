2b1786c33cc5005c33363b5a3f124cded2486a70
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-11-09 09:50:55 +0100

    Version 0.1.2 (#91)

--- mongox/__init__.py
+++ mongox/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from mongox.database import Client, Collection, Database
 from mongox.exceptions import InvalidKeyException, MultipleMatchesFound, NoMatchFound
